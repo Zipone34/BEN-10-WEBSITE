d8003d25c50a6ad7d28ee3faf29455e377886185
Zipone34 <115150262+Zipone34@users.noreply.github.com>
2024-09-16 14:28:02 +0800

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,13 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const navbg = document.querySelector('.nav-bg');
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    navbg.classList.toggle('active');
+});
+
+$(window).on("load", function() {
+    $(".loader-wrapper").fadeOut(5000);
+});
+
